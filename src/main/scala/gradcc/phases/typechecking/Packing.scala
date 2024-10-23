19ad2748d7f3ba067284c3c90886fe7029178a26
Valentin Aebi <valentin.aebi@epfl.ch>
2024-10-23 12:00:16 +0200

    Unpack shape only

--- src/main/scala/gradcc/phases/typechecking/Packing.scala
+++ src/main/scala/gradcc/phases/typechecking/Packing.scala
@@ -2,16 +2,14 @@ package gradcc.phases.typechecking
 
 import gradcc.lang.*
 
-private def unpackIfRecursive(tpe: Type, selfRef: Path): Type = tpe match {
+def unpackIfRecursive(tpe: Type, selfRef: Path): Type = tpe match {
   case Type(RecordShape(Some(selfVarId), fields), capSet) =>
     val substMap = Map[Capturable, Path](VarPath(selfVarId) -> selfRef)
     Type(
       RecordShape(None, fields.map(
         (fld, tpe) => (fld, substitute(tpe)(using substMap)))
       ),
-      capSet.map(
-        substitute(_)(using substMap)
-      )
+      capSet
     )
   case _ => tpe
 }
